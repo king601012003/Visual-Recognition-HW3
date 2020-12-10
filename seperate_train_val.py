50893dcf37838690df86aefbb291bced3a23b2f2
king601012003 <74351108+king601012003@users.noreply.github.com>
2020-12-10 21:37:27 +0800

    Add files via upload

---
+++ seperate_train_val.py
@@ -0,0 +1,59 @@
+import json
+import random
+from PIL import Image
+import numpy as np
+from tqdm import tqdm
+
+json_path = './pascal_train.json'
+val_img_num = 149
+
+
+train_imgs = []
+val_anno = []
+train_anno = []
+train_json = {}
+val_json = {}
+
+with open(json_path) as json_file: 
+    results = json.load(json_file)
+    
+    
+val_imgs = random.sample(results["images"], val_img_num)
+
+for img in results["images"]:
+    flag = False
+    for val_img in val_imgs:
+        if img == val_img:
+            flag = True
+            break
+        
+    if flag:
+        continue
+    
+    train_imgs.append(img)
+
+for anno in results["annotations"]:
+    anno_id = anno["image_id"]
+    flag = True
+    for val_img in val_imgs:
+        if anno_id == val_img["id"]:
+            val_anno.append(anno)
+            flag = False
+            break
+        
+    if flag:
+        train_anno.append(anno)
+        
+train_json["annotations"] = train_anno
+train_json["categories"] = results["categories"]
+train_json["images"] = train_imgs
+
+val_json["annotations"] = val_anno
+val_json["categories"] = results["categories"]
+val_json["images"] = val_imgs
+
+with open("./pascal_sbd_train.json", 'w') as js:
+    json.dump(train_json, js, ensure_ascii=False)
+    
+with open("./pascal_sbd_val.json", 'w') as js:
+    json.dump(val_json, js, ensure_ascii=False)
